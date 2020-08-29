866d8fc223c093d34a7d18b7049816d8baf610a9
Hardy <1277146050@qq.com>
2020-08-29 09:52:55 +0800

    [delivery][Hardy]  refactor: refactor  delivery

--- src/delivery.js
+++ src/delivery.js
@@ -1,41 +1,44 @@
-function deliveryDate (anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime;
-    if ([
+function rushDeliveryOrder(anOrder) {
+  let deliveryTime;
+  if ([
       'MA',
       'CT',
     ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
+    deliveryTime = 1;
+  } else if ([
       'NY',
       'NH',
     ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
-      deliveryTime = 3;
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+    deliveryTime = 2;
+  } else {
+    deliveryTime = 3;
   }
-  else {
-    let deliveryTime;
-    if ([
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function unrushDeliveryDate(anOrder) {
+  let deliveryTime;
+  if ([
       'MA',
       'CT',
       'NY',
     ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
+    deliveryTime = 2;
+  } else if ([
       'ME',
       'NH',
     ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    deliveryTime = 3;
+  } else {
+    deliveryTime = 4;
   }
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
+function deliveryDate(anOrder, isRush) {
+  return isRush ? rushDeliveryOrder(anOrder) : unrushDeliveryDate(anOrder);
+}
+
+module.exports = {
+  deliveryDate
 }
